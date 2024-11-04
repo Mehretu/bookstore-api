e7bef7596f988fdb70352e77d87dd79c90b3a27b
Mehretu <mehertu.abreham@gmail.com>
2024-11-04 09:41:13 +0300

    user registration route

--- services/auth-service/Routes/Auth.route.js
+++ services/auth-service/Routes/Auth.route.js
@@ -1,9 +1,27 @@
 const express = require('express')
 const { default: next } = require('next')
 const router = express.Router()
+const createError = require('http-errors')
+const User = require('../Models/User.model')
 
 router.post('/register', async(req,res,next) => {
-    res.send("register route")
+    console.log(req.body)
+    try {
+        const {email, password} = req.body
+        if(!email || !password) throw createError.BadRequest()
+        
+        const doesExist = await User.findOne({email: email})
+        if (doesExist) throw createError.Conflict(`${email} is already been registered`)
+        
+        const user = new User({email, password})
+        const savedUser = await user.save()
+
+        res.send(savedUser)
+        
+         
+    }catch(error){
+        next(error)
+    }
 })
 
 
