274d86c129f505bc05636ca1e563e39cb1e4d409
Mehretu <mehertu.abreham@gmail.com>
2024-11-04 11:35:09 +0300

    generate super key

--- services/auth-service/helpers/jwt_helper.js
+++ services/auth-service/helpers/jwt_helper.js
@@ -7,7 +7,7 @@ module.exports = {
             const payload = {
                 
             }
-            const secret = "come super secret"
+            const secret = process.env.ACCESS_TOKEN_SECRET
             const options = {
                 expiresIn: "1h",
                 issuer: "mehretu.com",
