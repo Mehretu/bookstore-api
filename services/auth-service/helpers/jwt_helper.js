6df7ba5efe3da9e2be63964bcf454006b60f02d5
Mehretu <mehertu.abreham@gmail.com>
2024-11-05 15:03:12 +0300

    minor mods

--- services/auth-service/helpers/jwt_helper.js
+++ services/auth-service/helpers/jwt_helper.js
@@ -14,7 +14,7 @@ module.exports = {
             }
             const secret = process.env.ACCESS_TOKEN_SECRET
             const options = {
-                expiresIn: "10m",
+                expiresIn: "30m",
                 issuer: "mehretu.com",
                 audience: userId
             }
