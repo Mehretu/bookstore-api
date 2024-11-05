743a3a20812b2f8d8522614caff57df822fc1c03
Mehretu <mehertu.abreham@gmail.com>
2024-11-05 10:39:51 +0300

    modified for indexing

--- services/book-service/src/Models/Book.model.js
+++ services/book-service/src/Models/Book.model.js
@@ -4,47 +4,74 @@ const BookSchema = new mongoose.Schema({
     title: {
         type: String,
         required: true,
-        trim: true
+        trim: true,
+        index: true
     },
     isbn: {
         type: String,
         required: true,
-        trim: true
+        unique: true,
+        trim: true,
     },
     author: {
         type: String,
         required: true,
+        index: true
     },
     description: {
         type: String,
         required: true,
+        
     },
     category: {
         type: String,
         required: true,
-        enum: ['FICTION', 'NON_FICTION', 'BIOGRAPHY', 'SELF_HELP', 'TECHNOLOGY','HISTORY','MATH','SCIENCE','RELIGION','OTHER']
+        enum: ['FICTION', 'NON_FICTION', 'BIOGRAPHY', 'SELF_HELP', 'TECHNOLOGY','HISTORY','MATH','SCIENCE','RELIGION','OTHER'],
+        index: true
     },
     price: {
         type: Number,
-        required: true
+        required: true,
+        index: true
+
     },
     metadata: {
         popularity: {
             rating: {
                 type:Number,
-                default:0
+                default:0,
+                index: true
             },
            reviewCount: {
                 type:Number,
                 default:0
             }
         },
-        genre: [String]
+        genre: [{
+            type: String,
+            index: true
+        }],
     }
 },{
     timestamps: true,
     toJSON: {
         virtuals: true
+    }
+})
+
+BookSchema.index({ category: 1, 'metadata.rating': -1 })
+BookSchema.index({ 'metadata.genre': 1, price: 1 })
+
+
+BookSchema.index({ 
+    title: 'text', 
+    description: 'text', 
+    author: 'text' 
+}, {
+    weights: {
+        title: 10,     
+        author: 5,      
+        description: 1  
     }
 })
 
